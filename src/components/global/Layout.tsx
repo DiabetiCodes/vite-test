64250db6d49b9472db6daacde9b46298c4075225
Michael Mastrucci <michael.mastrucci@gmail.com>
2022-10-27 14:49:02 -0400

    Added services 3 card

--- src/components/global/Layout.tsx
+++ src/components/global/Layout.tsx
@@ -2,14 +2,14 @@ import { Link } from 'react-router-dom'
 
 function Layout() {
   return (
-    <nav className='bg-teal-500'>
+    <nav className='bg-teal-500 drop-shadow-sm'>
       <div className='max-w-screen-2xl flex items-center justify-between flex-wrap bg-teal-500 p-6 mx-auto'>
         <div className='flex items-center flex-shrink-0 text-white mr-6 bg-teal-500'>
           <Link to='/'>
             LOGO
           </Link>
         </div>
-        <div className='block lg:hidden'>
+        <div className='block md:hidden'>
           <button className='flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white'>
           <svg className='fill-current h-3 w-3" viewBox="0 0 20 20' xmlns='http://www.w3.org/2000/svg'><title>Menu</title><path d='M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z'/></svg>
           </button>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,29 +1,50 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
-    <div className="bg-gray-200 ">
-    <div className="container px-6 py-4 mx-auto lg:flex lg:h-128 lg:py-16 ">
-    <div className="flex items-center justify-center w-full mt-2 lg:h-96 lg:w-1/2 order-1">
-            <img className="object-cover w-full max-w-2xl rounded-md lg:h-full"
-                src="https://source.unsplash.com/user/erondu/1600x900" alt="apple watch photo" />
-        </div>
-        <div className="flex flex-col items-center w-full lg:flex-row lg:w-1/2">
-            <div className="max-w-lg">
-                <h1 className="text-xl tracking-wide text-gray-800 lg:text-4xl">Set your
-                    Title</h1>
-                <p className="mt-4 text-gray-600">Lorem ipsum, dolor sit amet consectetur
-                    adipisicing elit. Aut quia asperiores alias vero magnam recusandae adipisci ad vitae
-                    laudantium quod rem voluptatem eos accusantium cumque.</p>
-                <div className="mt-6">
-                    <Link to="#"
-                        className="inline-block px-3 py-2 font-semibold text-center text-white transition-colors duration-200 transform bg-teal-500 rounded-md hover:dark:bg-gray-800">
-                        Get Started</Link>
-                </div>
+    <>
+      <div className='bg-gray-200 '>
+        <div className='container px-6 py-4 mx-auto lg:flex lg:h-128 lg:py-16 '>
+          <div className='flex items-center justify-center w-full mt-2 lg:h-96 lg:w-1/2 order-1'>
+            <img
+              className='object-cover w-full max-w-2xl rounded-md lg:h-full md:drop-shadow-md'
+              src='https://source.unsplash.com/user/erondu/1600x900'
+              alt='apple watch photo'
+            />
+          </div>
+          <div className='flex flex-col items-center w-full lg:flex-row lg:w-1/2'>
+            <div className='max-w-2xl lg:max-w-lg mt-8 lg:mt-0'>
+              <h1 className='text-xl tracking-wide text-gray-800 lg:text-4xl'>
+                Set your Title
+              </h1>
+              <p className='mt-4 text-gray-600'>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut quia
+                asperiores alias vero magnam recusandae adipisci ad vitae
+                laudantium quod rem voluptatem eos accusantium cumque.
+              </p>
+              <div className='mt-6'>
+                <Link
+                  to='#'
+                  className='inline-block px-3 py-2 font-semibold text-center text-white transition-colors duration-200 transform bg-teal-500 rounded-md hover:dark:bg-gray-800'
+                >
+                  Get Started
+                </Link>
+              </div>
             </div>
+          </div>
         </div>
-        
-    </div>
-</div>
-  )
+      </div>
+      <div className='container mx-auto lg:flex px-6 py-4 my-8 items-center justify-center w-full'>
+        <div className='flex flex-col items-center w-full lg:flex-row lg:w-1/3 justify-center'>
+          Lorem ipsum
+        </div>
+        <div className='flex flex-col items-center w-full lg:flex-row lg:w-1/3 justify-center'>
+          Lorem ipsum
+        </div>
+        <div className='flex flex-col items-center w-full lg:flex-row lg:w-1/3 justify-center'>
+          Lorem ipsum
+        </div>
+      </div>
+    </>
+  );
 }
